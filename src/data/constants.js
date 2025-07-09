29f45039d277bce0436bd7892902d642c46b4009
KARAN GOPLANI <karangoplani72@gmail.com>
2025-07-10 00:42:04 +0530

    Initial commit - React portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,261 @@
+import projectImg from "./image.png";
+import projectImg2 from "./project2.png";
+import projectImg3 from "./project3.png";
+export const Bio = {
+  name: "KARAN GOPLANI",
+  roles: [
+    "Front End Developer",
+    "Quality Analyst",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/karangoplani72",
+  resume:
+    "https://drive.google.com/file/d/1me1jdoPKFZVGtlpEf_ZRfdTf8x_yh_Sn/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/karangoplani72/",
+  twitter: "https://",
+  insta: "https://www.instagram.com/karangoplani72/",
+  facebook: "https://www.facebook.com/karangoplani72",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      }
+
+
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://www.pngfind.com/pngs/m/136-1363736_express-js-icon-png-transparent-png.png",
+      },
+
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+
+    ],
+  },
+
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/512px-Figma-logo.svg.png?20210804221519",
+      },
+      {
+        name: "Trello",
+        image: "https://cdn.worldvectorlogo.com/logos/trello.svg",
+      },
+      {
+        name: "Canva",
+        image: "https://www.vectorlogo.zone/logos/canva/canva-icon.svg",
+      },
+      {
+        name: "Wordpress",
+        image: "https://cdn.worldvectorlogo.com/logos/wordpress-2.svg",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/C510BAQHkUrQ4UAzNug/company-logo_200_200/company-logo_200_200/0/1631416109741?e=1757548800&v=beta&t=PZCIzpQaYRpnsT60ofoW9NaXrTX47Xlt63Yk4KyQmC8",
+    role: "Software Developer Intern",
+    company: "Artiset",
+    date: "January 2025 - Ongoing",
+    desc: "Worked on frontend tasks using HTML, CSS, and JavaScript. Gained hands-on experience in building responsive UI components and integrating APIs. Also focused on quality analysis by testing features, identifying bugs, and ensuring smooth user experience through regular feedback and improvements.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Manual Testing",
+      "Bug Reporting"
+    ],
+    doc: "https://drive.google.com/file/d/1rYkztfyuRoQTO1rypzdTgo89B85TmKpm/view?usp=drive_link"
+  }
+
+
+
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/D4D0BAQFxdG4Tc10X4w/company-logo_200_200/B4DZe_rz8FG8AI-/0/1751267626039/dpu_vidyapeeth_logo?e=1757548800&v=beta&t=rkXAjFbGcpcqmetBSVKBZoKEYFknCulK3-ls8e3UCy8",
+    school: "Dr. D. Y. Patil School of Science and Technology, Thatwade, Pune",
+    date: "Aug 2024 - Ongoing",
+    grade: "9.18 CGPA",
+    desc: "I am currently pursuing a Master's degree in Computer Applications (MCA) at DY Patil School of Science and Technology, Pune. I have maintained a strong academic record and have been recognized for my consistent performance. My coursework covers advanced topics such as Data Structures, Algorithms, Operating Systems, Computer Networks, Compiler Design, Blockchain Technology, and Data Analytics. I am focused on building a strong foundation in both theoretical concepts and practical applications in the field of computer science.",
+    degree: "Master of Computer Application",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/v2/D560BAQHzf5zb2S1q2g/company-logo_200_200/company-logo_200_200/0/1705218912731?e=1757548800&v=beta&t=blasfqwwmZVmljQorh04qmyaJvaSn4OoAEQIOKpYgKY",
+    school: "Shree H.N. Shukla Group of Colleges, Rajkot, Gujarat",
+    date: "Aug 2021 - July 2024",
+    grade: "First Distinction - CGPA 8.17",
+    desc: "I completed my Bachelor's degree in Computer Applications (BCA) from Shree H. N. Shukla Group of Colleges with a strong academic record. The program equipped me with a solid foundation in programming, web development, databases, and software engineering. It also helped me build strong logical thinking and problem-solving skills through hands-on projects and practical coursework.",
+    degree: "Bachelors of Computer Application",
+  },
+  {
+    id: 2,
+    img: "https://scontent.fpnq13-3.fna.fbcdn.net/v/t1.6435-1/66856294_219026932407817_5627274028189220864_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=105&ccb=1-7&_nc_sid=1d2534&_nc_ohc=xiH85iAedocQ7kNvwF3NgoB&_nc_oc=AdlSlLgb_CY9U4FBowBKXgUpwQda8AJ2isCZa0FHBwgphMpwVjM-j0Es9WVbU1y2lvW3EqI015LlaHw5QpM6Tvje&_nc_zt=24&_nc_ht=scontent.fpnq13-3.fna&_nc_gid=MLnV6OmqqSTGerZiYBzfiw&oh=00_AfS1WOznMMRZ469taBCQvS4CQ76ibGXPe7gxYc0dWxGHpw&oe=6895D1C2",
+    school: "Tapasvi School,Rajkot, Gujarat",
+    date: "June 2020 - March 2021",
+    grade: "69.20%",
+    desc: "I completed my 12th grade in Commerce from Tapasvi English Medium School, Rajkot, with a strong academic performance. During this time, I developed an interest in technology and business, which later motivated me to pursue a career in computer applications.",
+    degree: "HSC (XII), Commerce",
+  },
+  {
+    id: 3,
+    img: "https://scontent.fpnq13-1.fna.fbcdn.net/v/t39.30808-1/302078145_490295826437978_7373990499362775230_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=103&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=cEcT1WWVyVoQ7kNvwHfsC_l&_nc_oc=AdkhVke8x9bQB1GQEJNXx6ZdD9VFiaJgs0DFyHDfDaeF4W_u-xrDJ0MvJAiz2Vsoear7ygifao_H_hlpj2EHS31A&_nc_zt=24&_nc_ht=scontent.fpnq13-1.fna&_nc_gid=Jce6VPdl4FNftwFW_aMXUQ&oh=00_AfTgn7fAqbFyJl6RzwzwZqAqPdB2oqxohW3LOUPfmYA8Jg&oe=687443D5",
+    school: "Shree G.K. Dholakiya School,Rajkot, Gujarat",
+    date: "June 2018 - March 2019",
+    grade: "73.40%",
+    desc: "I completed my 10th grade from Shree G.K. Dholakiya School, Rajkot, with a strong academic performance. This foundational education helped me develop a keen interest in mathematics, which later influenced my choice to pursue computer applications.",
+    degree: "SSC (X)"
+  }
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "To Do Web App",
+    date: "March 2025",
+    description:
+      "",
+    image: projectImg,
+    tags: [
+      "React Js", "HTML", "CSS", "JavaScript", "Node Js", "Express Js", "MySQL"
+    ],
+    category: "web app",
+    github: "",
+    webapp: "https://drive.google.com/drive/folders/1z5-EDEj30gGs01v_Xqkk85aKEHyJyn_p?usp=sharing",
+    member: [
+      {
+        name: "Karan Goplani",
+        img: "https://avatars.githubusercontent.com/u/92224864?s=400&u=1d609fde501621d9b0758f5c117dfdfdcd9d7462&v=4",
+        linkedin: "https://www.linkedin.com/in/karangoplani72/",
+        github: "",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Personal Portfolio Website",
+    date: "April 2025",
+    description:
+      "A modern and responsive personal portfolio website built using React.js.",
+    image: projectImg,
+    tags: [
+      "React Js", "HTML", "CSS"
+    ],
+    category: "web app",
+    //github: "https://github.com/karangoplani72/Gamified_Coding_Platform.git",
+    //webapp: "https://drive.google.com/drive/folders/1z5-EDEj30gGs01v_Xqkk85aKEHyJyn_p?usp=sharing",
+    member: [
+      {
+        name: "Karan Goplani",
+        img: "https://avatars.githubusercontent.com/u/92224864?s=400&u=1d609fde501621d9b0758f5c117dfdfdcd9d7462&v=4",
+        linkedin: "https://www.linkedin.com/in/karangoplani72/",
+        github: "https://github.com/karangoplani72",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Nail Art Studio",
+    date: "MAY 2025",
+    description:
+      "A PHP-based dynamic website for a Nail Studio featuring service listings, appointment booking, and admin management.",
+    image: projectImg2,
+    tags: ["HTML", "CSS", "JavaScript", "MySQL", "PHP"],
+    category: "web app",
+    github: "",
+    member: [
+      {
+        name: "Karan Goplani",
+        img: "https://avatars.githubusercontent.com/u/92224864?s=400&u=1d609fde501621d9b0758f5c117dfdfdcd9d7462&v=4",
+        linkedin: "https://www.linkedin.com/in/karangoplani72/",
+        github: "https://github.com/karangoplani72",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Nail Art Studio",
+    date: "MAY 2025",
+    description:
+      "A responsive React.js website for a Nail Studio with service showcase, online booking, and interactive user interface.",
+    image: projectImg3,
+    tags: ["HTML", "CSS", "React Js", "nodejs", "MySQL"],
+    category: "web app",
+    github: "",
+    member: [
+      {
+        name: "Karan Goplani",
+        img: "https://avatars.githubusercontent.com/u/92224864?s=400&u=1d609fde501621d9b0758f5c117dfdfdcd9d7462&v=4",
+        linkedin: "https://www.linkedin.com/in/karangoplani72/",
+        github: "https://github.com/karangoplani72",
+      },
+    ],
+  }
+  //{
+  //new projects to be added here
+  //}
+];
